d190619529595f0f980620d538d881baa7e7d1ce
karimaFathi <karimafathifadili@gmail.com>
2024-05-06 12:54:51 +0100

    add DOM manipulation for UI interaction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice (){  
-    let textArray=['rock','paper','scissor'];
+    let textArray =['rock','paper','scissor'];
     let randomIndex = Math.floor(Math.random() * textArray.length); 
     let randomElement = textArray[randomIndex];
     return randomElement;
@@ -7,35 +7,80 @@ function getComputerChoice (){
 
 let countLose =0;
 let countWin = 0;
+let countRound = 0;
 
-    function playRound(ComputerSelection,PlayerSelection) {
-     if (ComputerSelection===PlayerSelection) {
-      alert( 'No one is the winner' );
-     } else if (
-      (PlayerSelection === 'rock' && ComputerSelection === 'scissors') ||
-      (PlayerSelection === 'paper' && ComputerSelection === 'rock') ||
-      (PlayerSelection === 'scissors' && ComputerSelection === 'paper')
-  ) {
-      countWin++;
-      alert('win');
-  } else {
-      countLose++;
-      alert('lose');
-  }
-}
-  
-function playGame(){
-    while(countLose + countWin != 5){
-    const ComputerSelection = getComputerChoice();
-    const PlayerSelection = prompt().toLowerCase();
-    playRound(ComputerSelection, PlayerSelection);
+    const btnRock = document.createElement('button');
+    const btnScissor = document.createElement('button');
+    const btnPaper = document.createElement('button');
+
+    btnRock.textContent= "rock";
+    btnRock.className = "playerSelectionBtn"
+    btnScissor.textContent= "scissor";
+    btnScissor.className = "playerSelectionBtn"
+    btnPaper.textContent= "paper";
+    btnPaper.className = "playerSelectionBtn"
+    const body = document.querySelector("body");
+    body.appendChild(btnRock);
+    body.appendChild(btnPaper);
+    body.appendChild(btnScissor);
+
+    const elements = document.querySelectorAll(".playerSelectionBtn");
+    
+    elements.forEach((btn)=>{
+      btn.style.display = "block";
+      btn.addEventListener("click",playRound);
+    })
+
+    function playRound(e) {
+ 
+    const computerSelection = getComputerChoice();
+    const playerSelection = e.target.textContent;
+    const gameOutput = document.createElement("div");
+    if (computerSelection===playerSelection) {
+    gameOutput.textContent ='draw';
+    } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissor') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissor' && computerSelection === 'paper')
+    ) {
+    countWin++;
+    gameOutput.textContent ='win';
+    } else {
+    countLose++;
+    gameOutput.textContent ='lose';     
     }
-      if(countWin > countLose){
-        alert("You Win finally!");
-      }
-      else {
-        alert("You lose unfortunatly!");
-      }  
-    }    
-
-playGame();
+    body.appendChild(gameOutput);
+    countRound++
+    if (countRound % 5 === 0) {
+        const finalGameOutput = document.createElement("div");
+        if(countWin > countLose){
+            finalGameOutput.textContent ='you win finaly';
+        } else if (countWin < countLose) {
+            finalGameOutput.textContent ='You lose unfortunatly!';
+        } else {
+            finalGameOutput.textContent ='no one is the winner';
+        }
+        body.appendChild(finalGameOutput);
+        countWin = 0;
+        countLose = 0;
+        // elements.forEach((btn) => {
+        //     btn.removeEventListener("click", playRound);
+        //   });
+        }
+       
+}
+// function playGame(){
+//     // while(countLose + countWin != 5){
+//     const ComputerSelection = getComputerChoice();
+//     const PlayerSelection = prompt().toLowerCase();
+//     playRound(ComputerSelection, PlayerSelection);
+//     // }
+//       if(countWin > countLose){
+//         alert("Yey U Win !");
+//       }
+//       else {
+//         alert("You lose unfortunatly!");
+//       }  
+//     }    
+     
+// playGame();
